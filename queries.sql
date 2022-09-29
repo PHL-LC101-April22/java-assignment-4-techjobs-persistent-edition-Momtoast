c38b9ae9fde85b34299f16e357ec24291525b83e
Melissa Matos <melissamomtoast@gmail.com>
2022-09-29 19:59:52 -0400

    part 2 working

--- queries.sql
+++ queries.sql
@@ -2,6 +2,8 @@
 -- The job table has id, employer, name and skills fields. Id is an int, the rest are varchar.
 
 -- Part 2: Test it with SQL
+SELECT * FROM techjobs.employer
+Where employer.location = "St. Louis";
 
 -- Part 3: Test it with SQL
 
