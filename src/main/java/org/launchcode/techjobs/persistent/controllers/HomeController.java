f600f0587368f3339064ca8a7084de7042ee0aa5
Melissa Matos <melissamomtoast@gmail.com>
2022-10-02 20:06:57 -0400

    step 4 not quite working

--- src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
+++ src/main/java/org/launchcode/techjobs/persistent/controllers/HomeController.java
@@ -2,8 +2,10 @@ package org.launchcode.techjobs.persistent.controllers;
 
 import org.launchcode.techjobs.persistent.models.Employer;
 import org.launchcode.techjobs.persistent.models.Job;
+import org.launchcode.techjobs.persistent.models.Skill;
 import org.launchcode.techjobs.persistent.models.data.EmployerRepository;
 import org.launchcode.techjobs.persistent.models.data.JobRepository;
+import org.launchcode.techjobs.persistent.models.data.SkillRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +13,7 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,6 +29,9 @@ public class HomeController {
     @Autowired
     JobRepository jobRepository;
 
+    @Autowired
+    SkillRepository skillRepository;
+
     @RequestMapping("")
     public String index(Model model) {
 
@@ -38,12 +44,13 @@ public class HomeController {
     public String displayAddJobForm(Model model) {
         model.addAttribute("title", "Add Job");
         model.addAttribute("employers", employerRepository.findAll());
+        model.addAttribute("skills", skillRepository.findAll());
         model.addAttribute(new Job());
         return "add";
     }
 
     @PostMapping("add")
-    public String processAddJobForm(@ModelAttribute @Valid Job newJob,
+    public String processAddJobForm(@ModelAttribute @Valid Job job,
                                        Errors errors, Model model,
                                     @RequestParam int employerId,
                                     @RequestParam(required=false) List<Integer> skills) {
@@ -55,14 +62,16 @@ public class HomeController {
             Optional<Employer> optEmployer = employerRepository.findById(employerId);
             if (optEmployer.isPresent()) {
                 Employer employer = (Employer) optEmployer.get();
-                newJob.setEmployer(employer);
-                jobRepository.save(newJob);
-            } else {
-                return "add";
+                job.setEmployer(employer);
             }
+            List<Skill> skillObjs = (List<Skill>) skillRepository.findAllById(skills);
+            if (skillObjs.size() > 0) {
+                job.setSkills(skillObjs);
+            }
+            jobRepository.save(job);
+            return "add";
         }
 
-        return "redirect:";
     }
 
     @GetMapping("view/{jobId}")
